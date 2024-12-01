9f250f1e0728cf3fdc8e2753b5ba8d49fbcce6e1
mikemell <mmell@hotmail.com>
2024-12-01 15:49:48 -0800

    Cleanup, initialized template. -msm

--- src/hello.cob
+++ src/hello.cob
@@ -1,9 +1,32 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. hello.
+
+       PROGRAM-ID.    HELLO.
+       AUTHOR.        MMELL.
+       INSTALLATION.  AUBURN.
+       DATE-WRITTEN.  NOV 29,2024.
+       DATE-COMPILED. NOV 29,2024.
+       SECURITY.      UNCLASSIFIED.
+
+      ***************************************************************** 
+      *                                                               * 
+      * THIS IS THE CLASSIC HELLO WORLD APPLICATION.                  * 
+      *                                                               * 
+      ***************************************************************** 
+
        ENVIRONMENT DIVISION.
+
        CONFIGURATION SECTION.
+
+       SOURCE-COMPUTER. X86_64.
+       OBJECT-COMPUTER. X86_64.
+
        DATA DIVISION.
+
+       FILE SECTION.
+
        WORKING-STORAGE SECTION.
+
        PROCEDURE DIVISION.
-           DISPLAY 'HELLO WORLD'.
+           DISPLAY 'HELLO WORLD!'.
            STOP RUN.
+
