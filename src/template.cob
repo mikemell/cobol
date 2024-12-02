e74e874ff9ae912a11c48d5523085f338096cf93
mikemell <mmell@hotmail.com>
2024-12-01 16:10:17 -0800

    -msm

--- src/template.cob
+++ src/template.cob
@@ -1,7 +1,3 @@
-PPPSSSCAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBIIIIIIII
-123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012
-                      |
-
 001010 IDENTIFICATION DIVISION.                                         PROGNAME
 001020                                                                  PROGNAME
 001030 PROGRAM-ID.    PROGNAME.                                         PROGNAME
@@ -11,16 +7,16 @@ PPPSSSCAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBIIIIIIII
 001070 DATE-COMPILED. NOV 29,2024.                                      PROGNAME
 001080 SECURITY.      UNCLASSIFIED.                                     PROGNAME
 001090                                                                  PROGNAME
-001100***************************************************************** PROGNAME
-001110*                                                               * PROGNAME
-001120* THIS PROGRAM PRODUCES A POLICY REPORT                         * PROGNAME
-001130*                                                               * PROGNAME
-001140***************************************************************** PROGNAME
-
+001100******************************************************************PROGNAME
+001110*                                                                *PROGNAME
+001120* THIS PROGRAM PRODUCES A POLICY REPORT                          *PROGNAME
+001130*                                                                *PROGNAME
+001140******************************************************************PROGNAME
+001150                                                                  PROGNAME
 002010 ENVIRONMENT DIVISION.                                            PROGNAME
 002020                                                                  PROGNAME
 002030 CONFIGURATION SECTION.                                           PROGNAME
-002040
+002040                                                                  PROGNAME
 002050 SOURCE-COMPUTER. X86_64.                                         PROGNAME
 002060 OBJECT-COMPUTER. X86_64.                                         PROGNAME
 002070                                                                  PROGNAME
@@ -30,7 +26,7 @@ PPPSSSCAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBIIIIIIII
 002110           ASSIGN TO UT-S-INPUT                                   PROGNAME
 002120     SELECT OUTPUT-FILE                                           PROGNAME
 002130           ASSIGN TO UT-S-PRINT                                   PROGNAME
-	  
+002140                                                                  PROGNAME	  
 003010 DATA DIVISION.                                                   PROGNAME
 003020                                                                  PROGNAME
 003030 FILE SECTION.                                                    PROGNAME
@@ -64,51 +60,51 @@ PPPSSSCAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBIIIIIIII
 003220                                                                  PROGNAME
 003230 01  PROGRAM-INDICATORS.                                          PROGNAME
 003240     05  ARE-THERE-MORE-RECORDS  PICTURE X(3) VALUE 'YES'.        PROGNAME
-
-Arithmetic - COMPUTE ADD SUBTRACT MULTIPLY DIVIDE
-Compiler directives - COPY REPLACE ENTER USE
-Conditional - EVALUATE IF
-Data movement - INITIALIZE INSPECT MOVE STRING UNSTRING
-Ending - STOP
-Input/output - ACCEPT CLOSE DELETE DISPLAY OPEN REWRITE START WRITE
-Interprocess communication - CALL CANCEL
-Ordering - MERGE RELEASE RETURN SORT
-Procedure/branching - ALTER EXIT GOTO PERFORM
-Table handling - SEARCH SET
-
-
-
+003250                                                                  PROGNAME
 004010 PROCEDURE DIVISION.                                              PROGNAME
-004020                                                                  PROGNAME
-004030***************************************************************** PROGNAME
-004040*                                                               * PROGNAME
-004050* THIS PROGRAM READS THE POLICY INPUT RECORDS AND CREATES THE   * PROGNAME
-004060* POLICY REPORT.  IT IS ENTERED FROM THE OPERATING SYSTEM AND   * PROGNAME
-004070* EXITS TO THE OPERATING SYSYTEM.                               * PROGNAME
-004080*                                                               * PROGNAME
-004090***************************************************************** PROGNAME
-004100                                                                  PROGNAME
-004110 A000-CREATE-POLICY-REPORT.                                       PROGNAME
-004120                                                                  PROGNAME
-004130     OPEN INPUT  INPUT-FILE                                       PROGNAME
-004140          OUTPUT REPORT-FILE.                                     PROGNAME
-004150     READ POLICY-INPUT-FILE                                       PROGNAME
-004160           AT END                                                 PROGNAME
-004170              MOVE 'NO ' TO ARE-THERE-MORE-RECORDS.               PROGNAME
-004180     PERFORM A001-FORMAT-PRINT-LINE                               PROGNAME
-004190           UNTIL ARE-THERE-MORE-RECORDS = 'NO '.                  PROGNAME
-004200     CLOSE INPUT-FILE                                             PROGNAME
-004210           REPORT-FILE.                                           PROGNAME
-004220     STOP RUN.                                                    PROGNAME
-
-005010 A001-FORMAT-PRINT-LINE.                                          PROGNAME
-005020     MOVE SPACES TO REPORT-LINE.                                  PROGNAME
-005030     MOVE POLICY-NUMBER-INPUT TO POLICY-NUMBER-REPORT.            PROGNAME
-005040     MOVE CUSTOMER-NAME-INPUT TO CUSTOMER-NAME-REPORT.            PROGNAME
-005050     MOVE AGENT-NAME-INPUT TO AGENT-NAME-REPORT.                  PROGNAME
-005060     MOVE INSURANCE-TYPE-INPUT TO INSURANCE-TYPE-REPORT.          PROGNAME
-005070     WRITE POLICY-REPORT-LINE                                     PROGNAME
-005080           AFTER ADVANCING 1 LINES.                               PROGNAME
-005090     READ POLICY-INPUT-FILE                                       PROGNAME
-005100           AT END                                                 PROGNAME
-005110              MOVE 'NO ' TO ARE-THERE-MORE-RECORDS.               PROGNAME
+004020******************************************************************PROGNAME
+004030* Arithmetic - COMPUTE ADD SUBTRACT MULTIPLY DIVIDE              *PROGNAME
+004040* Compiler directives - COPY REPLACE ENTER USE                   *PROGNAME
+004050* Conditional - EVALUATE IF                                      *PROGNAME
+004060* Data movement - INITIALIZE INSPECT MOVE STRING UNSTRING        *PROGNAME
+004070* Ending - STOP                                                  *PROGNAME
+004080* Input/output - ACCEPT CLOSE DELETE DISPLAY OPEN REWRITE START  *PROGNAME
+004090*      WRITE                                                     *PROGNAME
+004100* Interprocess communication - CALL CANCEL                       *PROGNAME
+004110* Ordering - MERGE RELEASE RETURN SORT                           *PROGNAME
+004120* Procedure/branching - ALTER EXIT GOTO PERFORM                  *PROGNAME
+004130* Table handling - SEARCH SET                                    *PROGNAME
+004140******************************************************************PROGNAME
+004150                                                                  PROGNAME
+004160******************************************************************PROGNAME
+004170*                                                                *PROGNAME
+004180* THIS PROGRAM READS THE POLICY INPUT RECORDS AND CREATES THE    *PROGNAME
+004190* POLICY REPORT.  IT IS ENTERED FROM THE OPERATING SYSTEM AND    *PROGNAME
+004200* EXITS TO THE OPERATING SYSYTEM.                                *PROGNAME
+004210*                                                                *PROGNAME
+004220******************************************************************PROGNAME
+004300                                                                  PROGNAME
+004310 A000-CREATE-POLICY-REPORT.                                       PROGNAME
+004320                                                                  PROGNAME
+004330     OPEN INPUT  INPUT-FILE                                       PROGNAME
+004340          OUTPUT REPORT-FILE.                                     PROGNAME
+004350     READ POLICY-INPUT-FILE                                       PROGNAME
+004360           AT END                                                 PROGNAME
+004370              MOVE 'NO ' TO ARE-THERE-MORE-RECORDS.               PROGNAME
+004380     PERFORM A001-FORMAT-PRINT-LINE                               PROGNAME
+004390           UNTIL ARE-THERE-MORE-RECORDS = 'NO '.                  PROGNAME
+004400     CLOSE INPUT-FILE                                             PROGNAME
+004410           REPORT-FILE.                                           PROGNAME
+004420     STOP RUN.                                                    PROGNAME
+004430                                                                  PROGNAME
+004440 A001-FORMAT-PRINT-LINE.                                          PROGNAME
+004450     MOVE SPACES TO REPORT-LINE.                                  PROGNAME
+004460     MOVE POLICY-NUMBER-INPUT TO POLICY-NUMBER-REPORT.            PROGNAME
+004470     MOVE CUSTOMER-NAME-INPUT TO CUSTOMER-NAME-REPORT.            PROGNAME
+004480     MOVE AGENT-NAME-INPUT TO AGENT-NAME-REPORT.                  PROGNAME
+004490     MOVE INSURANCE-TYPE-INPUT TO INSURANCE-TYPE-REPORT.          PROGNAME
+004500     WRITE POLICY-REPORT-LINE                                     PROGNAME
+004510           AFTER ADVANCING 1 LINES.                               PROGNAME
+004520     READ POLICY-INPUT-FILE                                       PROGNAME
+004530           AT END                                                 PROGNAME
+004540              MOVE 'NO ' TO ARE-THERE-MORE-RECORDS.               PROGNAME
